0fcf65bb1ea552fc3e0450960d28bb8003aafb2f
srz_zumix <zumix.cpp@gmail.com>
2024-04-07 21:15:01 +0900

    first

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euox pipefail
+
+source "${GITHUB_ACTION_PATH:-.}/resolve.sh"
+
+AUTOMAKE_TEMPDIR="${INPUTS_AUTOMAKE_TEMPDIR:-}"
+if [ -z "${AUTOMAKE_TEMPDIR}" ]; then
+  if [ -n "${RUNNER_TEMP}" ]; then
+    AUTOMAKE_TEMPDIR="${RUNNER_TEMP}"
+  else
+    AUTOMAKE_TEMPDIR="$(mktemp -d)"
+  fi
+fi
+
+AUTOMAKE_INSTALLDIR="${RUNNER_TOOL_CACHE:-${AUTOMAKE_TEMPDIR}}/automake/${VERSION}"
+
+mkdir -p "${AUTOMAKE_INSTALLDIR}"
+
+download() {
+  curl --output "${AUTOMAKE_TEMPDIR}/automake.tar.gz" -sSL "http://ftpmirror.gnu.org/automake/automake-${VERSION}.tar.gz"
+  curl --output "${AUTOMAKE_TEMPDIR}/automake.tar.gz.sig" -sSL "http://ftpmirror.gnu.org/automake/automake-${VERSION}.tar.gz.sig"
+  gpg --verify "${AUTOMAKE_TEMPDIR}/automake.tar.gz.sig" "${AUTOMAKE_TEMPDIR}/automake.tar.gz"
+  tar -zx -f "${AUTOMAKE_TEMPDIR}/automake.tar.gz" -C "${AUTOMAKE_TEMPDIR}/"
+
+}
+
+install() {
+  if [ ! -f "${AUTOMAKE_INSTALLDIR}/bin/automake" ]; then
+    download
+    cd "${AUTOMAKE_TEMPDIR}/automake-${VERSION}"
+    ./configure --prefix="${AUTOMAKE_INSTALLDIR}"
+    make -j"$(nproc)"
+    make install
+  fi
+}
+
+echo '::group::📖 Installing automake ...'
+install
+echo '::endgroup::'
---
+++ resolve.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+VERSION="${INPUTS_VERSION:-latest}"
+
+versions() {
+  curl -sSL ftp://ftp.gnu.org/gnu/automake/ | grep -o 'automake-[0-9.]*.tar.gz' | grep -o '[0-9.]*[0-9]' | sort -V
+}
+
+resolve_version() {
+  if [ "${VERSION}" == 'latest' ]; then
+    VERSION=$(versions | tail -1)
+  else
+    VERSION=$(versions | grep "^${VERSION}\$" | tail -1)
+  fi
+}
+
+WILDCARD_CHAR=\*
+if [ "${VERSION}" == 'latest' ] || (echo "${VERSION}" | grep '*'); then
+  echo '::group::📖 Resolve automake version ...'
+  resolve_version
+  echo '::endgroup::'
+fi
+
+echo "version=${VERSION}" >> "${GITHUB_OUTPUT:-/dev/null}"
