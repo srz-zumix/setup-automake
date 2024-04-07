df356eaaf889632ae797b92c9bcba6a73e1719f2
srz_zumix <zumix.cpp@gmail.com>
2024-04-08 00:15:00 +0900

    cache

--- resolve.sh
+++ resolve.sh
@@ -22,3 +22,16 @@ if [ "${VERSION}" == 'latest' ] || (echo "${VERSION}" | grep '[*]'); then
 fi
 
 echo "version=${VERSION}" | tee -a "${GITHUB_OUTPUT:-/dev/null}"
+
+AUTOMAKE_TEMPDIR="${INPUTS_AUTOMAKE_TEMPDIR:-}"
+if [ -z "${AUTOMAKE_TEMPDIR}" ]; then
+  if [ -n "${RUNNER_TEMP:-}" ]; then
+    AUTOMAKE_TEMPDIR="${RUNNER_TEMP:-}"
+  else
+    AUTOMAKE_TEMPDIR="$(mktemp -d)"
+  fi
+fi
+
+AUTOMAKE_INSTALLDIR="${RUNNER_TOOL_CACHE:-${AUTOMAKE_TEMPDIR}}/automake/${VERSION}"
+
+echo "path=${AUTOMAKE_INSTALLDIR}" | tee -a "${GITHUB_OUTPUT:-/dev/null}"
