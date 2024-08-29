962a3d7cb1019a134273f4569b0a4eb4ea07fcd5
srz_zumix <zumix.cpp@gmail.com>
2024-08-29 22:22:12 +0900

    fix

--- install-gnu-tool.sh
+++ install-gnu-tool.sh
@@ -3,18 +3,18 @@ set -euo pipefail
 
 TOOL_NAME=$1
 TARGET_VERSION=$2
-TARGET_TARGET_TOOL_INSTALLDIR="${TOOL_INSTALLDIR}/${TOOL_NAME}"
+TARGET_TOOL_INSTALLDIR="${TOOL_INSTALLDIR}/${TOOL_NAME}"
 TARGET_TOOL_TEMPDIR="${TOOL_TEMPDIR}/${TOOL_NAME}"
 
 mkdir -p "${TOOL_TEMPDIR}"
-mkdir -p "${TARGET_TARGET_TOOL_INSTALLDIR}"
+mkdir -p "${TARGET_TOOL_INSTALLDIR}"
 
 download() {
-  echo '::group::📖 Download ${TOOL_NAME} ...'
+  echo "::group::📖 Download ${TOOL_NAME} ..."
   curl --output "${TARGET_TOOL_TEMPDIR}/${TOOL_NAME}.tar.gz" -sSL "http://ftpmirror.gnu.org/${TOOL_NAME}/${TOOL_NAME}-${TARGET_VERSION}.tar.gz"
   curl --output "${TARGET_TOOL_TEMPDIR}/${TOOL_NAME}.tar.gz.sig" -sSL "http://ftpmirror.gnu.org/${TOOL_NAME}/${TOOL_NAME}-${TARGET_VERSION}.tar.gz.sig"
   echo '::endgroup::'
-  echo '::group::📖 Unarchive ${TOOL_NAME} ...'
+  echo "::group::📖 Unarchive ${TOOL_NAME} ..."
   gpg --verify "${TARGET_TOOL_TEMPDIR}/${TOOL_NAME}.tar.gz.sig" "${TARGET_TOOL_TEMPDIR}/${TOOL_NAME}.tar.gz"
   tar -zx -f "${TARGET_TOOL_TEMPDIR}/${TOOL_NAME}.tar.gz" -C "${TARGET_TOOL_TEMPDIR}/"
   echo '::endgroup::'
@@ -23,7 +23,7 @@ download() {
 install() {
   if [ ! -f "${TARGET_TOOL_INSTALLDIR}/bin/${TOOL_NAME}" ]; then
     download
-    echo '::group::📖 Installing ${TOOL_NAME} ...'
+    echo "::group::📖 Installing ${TOOL_NAME} ..."
     cd "${TARGET_TOOL_TEMPDIR}/${TOOL_NAME}-${TARGET_VERSION}"
     ./configure --prefix="${TARGET_TOOL_INSTALLDIR}"
     make -j"$(nproc)"
