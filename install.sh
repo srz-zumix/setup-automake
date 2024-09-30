ee259084c8d6a7f6ea24fea43062349442b6068b
srz_zumix <zumix.cpp@gmail.com>
2024-10-01 00:28:59 +0900

    log

--- install.sh
+++ install.sh
@@ -5,10 +5,17 @@ source "${GITHUB_ACTION_PATH:-.}/resolve.sh"
 
 mkdir -p "${AUTOMAKE_INSTALLDIR}"
 
+report() {
+  # Report log
+  echo "::group::📋 Report"
+  cat config.log || true
+  echo "::endgroup::"
+}
+
 install() {
   echo '::group::📖 Installing automake ...'
   cd "${AUTOMAKE_TEMPDIR}/automake-${VERSION}"
-  ./configure --prefix="${AUTOMAKE_INSTALLDIR}"
+  ./configure --prefix="${AUTOMAKE_INSTALLDIR}" || (report && exit 1)
   make -j"$(nproc)"
   make install
   echo '::endgroup::'
